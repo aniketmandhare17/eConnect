7ddcede89374457a84b41c7a0396b6fe48751009
Acer <Acer@DESKTOP-3HK22D0>
2021-04-30 00:00:16 -0700

    Empolyee New  file added

---
+++ NewComapny/NewCompany/Controllers/EmplyeeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewCompany.Controllers
+{
+    public class EmplyeeController : Controller
+    {
+        // GET: Emplyee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult EmployeeView()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ NewComapny/NewCompany/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewCompany.Models
+{
+    public class Employee
+    {
+
+        public String Eid { get; set; }
+
+
+        public String Fname { get; set; }
+
+        public String Lname { get; set; }
+        public String pos { get; set; }
+     
+
+    }
+}
